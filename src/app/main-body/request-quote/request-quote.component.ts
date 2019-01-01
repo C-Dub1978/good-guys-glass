92801d8a371d30c1bbb4543ad8bed66f2555874e
TransientCW <chris@myubercode.com>
2019-01-01 12:55:04 -0700

    moving onto vehicle quote generator scaffolding

--- src/app/main-body/request-quote/request-quote.component.ts
+++ src/app/main-body/request-quote/request-quote.component.ts
@@ -6,10 +6,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-quote.component.css']
 })
 export class RequestQuoteComponent implements OnInit {
+  years: number[];
+  makesAndModels: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    this.years = [];
+    const beginning = 1980;
+    const currentYear = new Date().getFullYear();
+    for (let i = beginning; i <= +currentYear; i++) {
+      this.years.push(i);
+    }
 
+    this.makesAndModels = [
+      {'Acura': []},
+      {'Alfa Romeo': []},
+      {'Aston Martin': []},
+      {'Audi': []},
+      {'BMW': []},
+      {'Buick': []},
+      {'Cadillac': []},
+      {'Chevrolet': []},
+      {'Chrysler': []},
+      {'Dodge': []},
+      {'Fiat': []},
+      {'Ford': []},
+      {'GMC': []},
+      {'Honda': []},
+      {'Hyundai': []},
+      {'Hummer': []},
+      {'Infiniti': []},
+      {'Jaguar': []},
+      {'Jeep': []},
+      {'Kia': []},
+      {'Land Rover': []},
+      {'Lexus': []},
+      {'Maserati': []},
+      {'Mazda': []},
+      {'Mercedes-Benz': []},
+      {'Mini': []},
+      {'Mitsubishi': []},
+      {'Nissan': []},
+      {'Other': null}
+      {'Porsche': []},
+      {'Saab': []},
+      {'Subaru': []},
+      {'Suzuki': []},
+      {'Tesla': []},
+      {'Toyota': []},
+      {'Volkswagen': []},
+      {'Volvo': []}
+    ];
+  }
 }
